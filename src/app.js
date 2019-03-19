c12ed9718b228e1e82f4fadc8e98ed4e4da5ae47
Marissa Douglass <mahdouglass@gmail.com>
2019-03-18 21:28:04 -0400

    Imported the table component into app.js where some other text is showing.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react"
+import Table from "./table"
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
+            <div className="container">
                 <h1>Hello World!</h1>
+                <Table />
             </div>
         )
     }
