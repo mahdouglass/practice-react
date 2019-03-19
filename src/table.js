c5c7dd9e53ac72a467dc6e055a166c34a520926d
Marissa Douglass <mahdouglass@gmail.com>
2019-03-18 21:18:05 -0400

    Created table component.

---
+++ src/table.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react"
+
+class Table extends Component {
+    render() {
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Job</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Charlie</td>
+                    <td>Systems Analyst</td>
+                </tr>
+                <tr>
+                    <td>Mac</td>
+                    <td>PHP Developer</td>
+                </tr>
+            </tbody>
+        </table>
+    }
+}
+
+export default Table
