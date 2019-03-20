4ca989d78dabef7324f1cd50d5167b2f34978272
Marissa Douglass <mahdouglass@gmail.com>
2019-03-19 21:17:19 -0400

    Removed data from the table's simple components.

--- src/table.js
+++ src/table.js
@@ -12,42 +12,11 @@ class Table extends Component {
 }
 
 const TableHeader = () => {
-    return (
-        <thead>
-            <tr>
-                <th scope="col"></th>
-                <th scope="col">Name</th>
-                <th scope="col">Job</th>
-            </tr>
-        </thead>
-    )
+    return <thead />
 }
 
 const TableBody = () => {
-    return (
-        <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>Charlie</td>
-                <td>Janitor</td>
-            </tr>
-            <tr>
-                <th scope="row">2</th>
-                <td>Mac</td>
-                <td>Bouncer</td>
-            </tr>
-            <tr>
-                <th scope="row">3</th>
-                <td>Dee</td>
-                <td>Aspiring actress</td>
-            </tr>
-            <tr>
-                <th scope="row">4</th>
-                <td>Dennis</td>
-                <td>Bartender</td>
-            </tr>
-        </tbody>
-    )
+    return <tbody />
   }
 
 export default Table
