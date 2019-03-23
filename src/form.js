077a34edcc95f559a2a10ae4455ddfce429b88a4
Marissa Douglass <mahdouglass@gmail.com>
2019-03-22 22:09:23 -0400

    Started to add the ability for forms to update state that will then pass that data to app then table.

--- src/form.js
+++ src/form.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react'
 
 class Form extends Component {
+    constructor(props) {
+        super(props)
 
+        this.initialState = {
+            name: '',
+            job: '',
+        }
+
+        this.state= this.initialState
+    }
 }
 
 export default Form
