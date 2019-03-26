e5ac3b84c9b91702036bcd4e5c9e5f34fedc2543
Marissa Douglass <mahdouglass@gmail.com>
2019-03-25 21:25:14 -0400

    Added wikipedia url api, put it in a generic function since consts throw an error outside of one.

--- src/api.js
+++ src/api.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react"
 
 class App extends Component {
+    state = {
+        data: [],
+    }
+    function() {
+        const url="https://en.wikipedia.org/w/api.php?action=opensearch&search=astronomy&format=json&origin=*"
+    }
+    
+
     render() {
         return (
             <div className="container">
