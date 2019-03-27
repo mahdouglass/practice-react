0e5832460c696ed65d2e81269bdbe81a4654675f
Marissa Douglass <mahdouglass@gmail.com>
2019-03-26 22:14:05 -0400

    Added a loading optionto show if the api call takes too long.

--- src/api.js
+++ src/api.js
@@ -3,19 +3,28 @@ import React, { Component } from "react"
 class App extends Component {
     state = {
         data: [],
+        isLoading: false,
     }
 
     // Code is run after component has been added to DOM
     componentDidMount() {
         const url="https://en.wikipedia.org/w/api.php?action=opensearch&search=astronomy&format=json&origin=*"
 
+        this.setState({ isLoading: true })
+
         fetch(url)
             .then(response => response.json())
-            .then(result => this.setState({ data: result }))
+            .then(result => this.setState({ 
+                data: result,
+                isLoading: false
+             }))
     }
 
     render() {
-        const { data } = this.state
+        const { data, isLoading } = this.state
+        if (isLoading) {
+            return <p>Loading...</p>
+        }
         const result = data.map((entry, index) => {
             return <li key={index}>{entry}</li>
         })
