1a12bb63ff80deb703ceb6c5b83128a00480eef8
Marissa Douglass <mahdouglass@gmail.com>
2019-03-28 21:06:07 -0400

    Remove api.js practice file and redirect to app again.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import ReactDom from "react-dom"
-// import App from "./app"
-import App from "./api"
+import App from "./app"
 import "./index.css"
 
 ReactDom.render(<App />, document.getElementById("root"))
