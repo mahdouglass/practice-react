71b07521e82c87662e47c72a166cd065f7073a6d
Marissa Douglass <mahdouglass@gmail.com>
2019-03-28 21:15:18 -0400

    Returning a button for the squares of tic tac toe so we can add click functionality.

---
+++ src/square.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Square extends Component {
+    render() {
+        return (
+            <button className="square">
+            </button>
+        )
+    }
+}
