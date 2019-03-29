4504a04871e884c3f51cf5c5746c7002d105dac7
Marissa Douglass <mahdouglass@gmail.com>
2019-03-28 21:29:23 -0400

    Set up all the rendering (did a lot of this tutorial out of order). Can now see a board set up with each of the values being passed through.

--- src/app.js
+++ src/app.js
@@ -1,33 +1,12 @@
 import React, { Component } from "react"
-import Table from "./table"
-import Form from "./form"
+import Game from "./game"
 
 class App extends Component {
-    state = {
-        characters: []
-    }
-
-    removeCharacter = index => {
-        const { characters } = this.state
-
-        this.setState({
-            characters: characters.filter((character, i) => {
-                return i !== index
-            })
-        })
-    }
-
-    handleSubmit = character => {
-        this.setState({ characters: [...this.state.characters, character]})
-    }
 
     render() {
-        const { characters } = this.state
         return (
             <div className="container">
-                <h1>Hello World!</h1>
-                <Form handleSubmit={this.handleSubmit} />
-                <Table characterData={characters} removeCharacter={this.removeCharacter} />
+               <Game />
             </div>
         )
     }
