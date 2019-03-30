2b6bc49b8ef121673ee2ddec1bdb5b41df9a71c8
Marissa Douglass <mahdouglass@gmail.com>
2019-03-29 23:02:16 -0400

    Created a constructor that passes in super since we're defining a constructor of a subclass where we are planning on saving the state of the buttons.

--- src/square.js
+++ src/square.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 
 class Square extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
     render() {
         return (
             <button className="square" onClick={() => alert('click')}>
                 {this.props.value}
             </button>
-        )
+        );
     }
 }
 export default Square;
