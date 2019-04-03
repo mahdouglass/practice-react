bc379747816c4701a47c31d92f677dbf9eff7648
Marissa Douglass <mahdouglass@gmail.com>
2019-04-02 21:58:18 -0400

    Now printing out if there is a winner and who it is.

--- src/board.js
+++ src/board.js
@@ -29,7 +29,13 @@ class Board extends Component {
     }
 
     render() {
-        const status = "Next player: " + (this.state.xIsNext ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
         return (
             <div>
                 <div className="status">{status}</div>
