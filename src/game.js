cd7253a7b2e14e3558243bc4ed61bd4e093d56f5
Marissa Douglass <mahdouglass@gmail.com>
2019-04-03 21:35:54 -0400

    Lifting up Board's state so that game component has full control so we can render previous turns from the history.

--- src/game.js
+++ src/game.js
@@ -2,6 +2,16 @@ import React, { Component } from "react";
 import Board from "./board";
 
 class Game extends Component { 
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            xIsNext: true,
+        }
+    }
+
     render () {
         return (
             <div className="game">
