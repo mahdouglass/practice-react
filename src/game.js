713e4c178ce7e4d951f3269d4f116e5bdddf20d6
Marissa Douglass <mahdouglass@gmail.com>
2019-04-04 21:10:28 -0400

    Recopied and retyped transition from board to game since I felt that I had missed something. That was the right thing to do, handleClick needed some updates to the history part.

--- src/game.js
+++ src/game.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Board from "./board";
+import Square from "./square";
 
 class Game extends Component { 
     constructor(props) {
@@ -13,37 +14,47 @@ class Game extends Component {
     }
 
     handleClick(i) {
-        const history = this.state.history;
-        const current = history[history.length - 1];
-        const squares = current.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
-          return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-          history: history.concat([{
-            squares: squares
-          }]),
-          xIsNext: !this.state.xIsNext,
-        });
-    }    
+      const history = this.state.history;
+      const current = history[history.length - 1];
+      const squares = current.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        history: history.concat([{
+          squares: squares,
+        }]),
+        xIsNext: !this.state.xIsNext,
+      });
+    }   
 
-    render () {
-        const winner = calculateWinner(this.state.squares);
-        let status;
-        if (winner) {
-            status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-        return (
-            <div className="game">
-                <div className="game-board">
-                    <div className="status">{status}</div>
-                    <Board />
-                </div>
-            </div>
-        );
+    render() {
+      const history = this.state.history;
+      const current = history[history.length - 1];
+      const winner = calculateWinner(current.squares);
+  
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
+  
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board
+              squares={current.squares}
+              onClick={(i) => this.handleClick(i)}
+            />
+          </div>
+          <div className="game-info">
+            <div>{status}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
     }
 }
 
