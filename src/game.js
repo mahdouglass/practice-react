334dd4168f5ae3fb4111f64d13c471ff3f73c585
Marissa Douglass <mahdouglass@gmail.com>
2019-04-07 21:48:00 -0400

    Changed verbiage to reset game to make it a little more clear.

--- src/game.js
+++ src/game.js
@@ -44,7 +44,7 @@ class Game extends Component {
       const winner = calculateWinner(current.squares);
       const moves = history.map((step, move) =>{
         const desc = move ?
-          'Go to move #' + move : 'Go to game start';
+          'Go to move #' + move : 'Reset game';
         return (
           <li key={move}>
             <button onClick={() => this.jumpTo(move)}>{desc}</button>
