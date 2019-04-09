67678a5dc87ee98794bb5849ec157ea5e5a1d64f
Marissa Douglass <mahdouglass@gmail.com>
2019-04-08 22:34:28 -0400

    Added simple form that can later be styled and have success/error messages.

--- src/form.js
+++ src/form.js
@@ -4,6 +4,37 @@ class Form extends Component {
     render() {
         return (
             <form>
+                <div className="form-group">
+                    <label>
+                        Name:
+                        <input type="text" placeholder="Name" />
+                    </label>
+                </div>
+                <div className="form-group">
+                    <label>
+                        Email:
+                        <input type="text" placeholder="Email" />
+                    </label>
+                </div>
+                <div className="form-group">
+                    <label>
+                        Street:
+                        <input type="text" placeholder="Street" />
+                    </label>
+                    <label>
+                        City:
+                        <input type="text" placeholder="City" />
+                    </label>
+                    <label>
+                        State:
+                        <input type="text" placeholder="State" />
+                    </label>
+                    <label>
+                        Zipcode:
+                        <input type="text" placeholder="Zipcode" />
+                    </label>
+                </div>
+                <button type="submit">Submit</button>
             </form>
         )
     }
