bfa5a599968f6db90f1e42b709650dccfb5ba4fb
Marissa Douglass <mahdouglass@gmail.com>
2019-04-09 10:37:39 -0400

    Added onSubmit code that will take the value and pass it through an alert.

--- src/form.js
+++ src/form.js
@@ -7,15 +7,21 @@ class Form extends Component {
             value: '',
         }
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
         this.setState({value: event.target.value});
     }
 
+    handleSubmit(event) {
+        alert("A name was submitted: " + this.state.value);
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
                     <label>
                         Name:
