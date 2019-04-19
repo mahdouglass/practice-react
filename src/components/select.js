12632b3748cefb9ac7ef19aa6af18310f6e98336
Marissa Douglass <mahdouglass@gmail.com>
2019-04-18 21:51:59 -0400

    Added blank attributes that we'll be using on the select element.

--- src/components/select.js
+++ src/components/select.js
@@ -3,7 +3,11 @@ import React from "react";
 const Select = (props) => {
     return(
         <div className="form-group">
-            <select>
+            <select
+                name=""
+                value=""
+                onChange=""
+            >
                 <option></option>
             </select>
         </div>
