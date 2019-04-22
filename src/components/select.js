7f556538021e4a1466f1fbd5f06f1512b828f5e7
Marissa Douglass <mahdouglass@gmail.com>
2019-04-21 21:58:52 -0400

    Attempting to pass values into options for the select dropdown. The mapping in options wasn't working, couldn't find additional documentation. Right now I'm passing in the options constant to the attribute. Not showing anything yet.

--- src/components/select.js
+++ src/components/select.js
@@ -1,5 +1,11 @@
 import React from "react";
 
+const options = [
+    { value: 'Alabama', label: 'Alabama' },
+    { value: 'Alaska', label: 'Alaska' },
+    { value: 'Arizona', label: 'Arizona' }
+];
+
 const Select = (props) => {
     return(
         <div className="form-group">
@@ -7,8 +13,8 @@ const Select = (props) => {
                 name={props.name}
                 value={props.value}
                 onChange={props.handleChange}
+                options={options}
             >
-                <option disabled>{props.placeholder}</option>
             </select>
         </div>
     )
