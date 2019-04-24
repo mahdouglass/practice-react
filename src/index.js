673eb9fc6a89fe964ebbd78b3d12543fa628605b
Marissa Douglass <mahdouglass@gmail.com>
2019-04-23 22:28:34 -0400

    Readded serviceWorker.js, it's import, and call

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from "react"
 import ReactDom from "react-dom"
 import App from "./app"
 import "./index.css"
+import * as serviceWorker from "./serviceWorker"
 
-ReactDom.render(<App />, document.getElementById("root"))
+ReactDom.render(<App />, document.getElementById("root"));
+serviceWorker.unregister();
