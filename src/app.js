6fc55e655682c509056495046c076e869d488edf
Marissa Douglass <mahdouglass@gmail.com>
2019-04-24 22:09:03 -0400

    Created person component
    Basic function component for demo purposes.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react"
+import "./index.css";
+import Person from "./person/person";
 
 class App extends Component {
 
     render() {
         return (
             <div className="container">
+                <Person />
             </div>
         )
     }
