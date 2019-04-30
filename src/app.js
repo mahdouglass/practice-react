de30aaa5fc39f9dfd1550904ec592186772a8b10
Marissa Douglass <mahdouglass@gmail.com>
2019-04-29 22:30:17 -0400

    Switched to class component, added method that we pass down through onChange in an input field

--- src/app.js
+++ src/app.js
@@ -1,40 +1,55 @@
-import React, { useState } from "react"
+import React, { Component } from "react"
 import "./index.css";
 import Person from "./person/person";
 
-const App = props => {
-    const [ personsState, setPersonState] = useState({
+class App extends Component {
+    state = {
         persons: [
             { name: "Lucy", age: 20 },
-            { name: "Hazel", age: 25 }
+            { name: "Hazel", age: 25 },
+            { name: "Harley", age: 17}
         ],
         otherState: 'another value'
-    });
+    };
 
-    const [otherState, setOtherState] = useState("some other value");
-    console.log(personsState, otherState);
-
-    const switchNameHandler = (newName) => {
-        setPersonState({
+    switchNameHandler = (newName) => {
+        this.setState = {
             persons: [
                 { name: "Lucy", age: 22 },
-                { name: newName, age: 25 }
+                { name: newName, age: 25 },
+                { name: "Rosco", age: 17}
             ]
-        });
+        };
     }
 
-    return (
-        <div className="container">
-            <button onClick={() => this.switchNameHandler("Hazel")}>Switch Name</button>
-            <Person 
-                name={personsState.persons[0].name} 
-                age={personsState.persons[0].age} />
-            <Person 
-                name={personsState.persons[1].name} 
-                age={personsState.persons[1].age}
-                click={switchNameHandler.bind(this, "Hazel Hazel")}>I like to run.</Person>
-        </div>
-    )
+    nameChangedHandler = (event) => {
+        this.setState = {
+            persons: [
+                { name: "Lucy", age: 22 },
+                { name: "Hazel", age: 25 },
+                { name: event.target.value, age: 17 },
+            ]
+        };
+    }
+
+    render () {
+        return (
+            <div className="container">
+                <button onClick={() => this.switchNameHandler("Hazel")}>Switch Name</button>
+                <Person 
+                    name={this.state.persons[0].name} 
+                    age={this.state.persons[0].age} />
+                <Person 
+                    name={this.state.persons[1].name} 
+                    age={this.state.persons[1].age}
+                    click={this.switchNameHandler.bind(this, "Hazel Hazel")}>I like to run.</Person>
+                <Person 
+                    name={this.state.persons[2].name} 
+                    age={this.state.persons[2].age}
+                    changed={this.nameChangedHandler} />
+            </div>
+        )
+    }
 }
 
 export default App;
