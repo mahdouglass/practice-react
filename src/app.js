f896c0eaeb92fb0eb97650367966188d87544f51
Marissa Douglass <mahdouglass@gmail.com>
2019-05-02 16:49:03 -0400

    Move toggle jsx content to variable
    When the button changes state of persons, it will re-render the content first by going through the if statement to determine if it will be shown or not.

--- src/app.js
+++ src/app.js
@@ -47,27 +47,32 @@ class App extends Component {
             cursor: "pointer"
         };
 
+        let persons = null;
+
+        if (this.state.showPersons) {
+            persons = (
+                <div>
+                    <Person 
+                        name={this.state.persons[0].name} 
+                        age={this.state.persons[0].age} />
+                    <Person 
+                        name={this.state.persons[1].name} 
+                        age={this.state.persons[1].age}
+                        click={this.switchNameHandler.bind(this, "Hazel Hazel")}
+                        changed={this.nameChangedHandler}>I like to run.</Person>
+                    <Person 
+                        name={this.state.persons[2].name} 
+                        age={this.state.persons[2].age} />
+                </div>
+            );
+        }
+
         return (
             <div className="container">
                 <button 
                     onClick={this.togglePersonsHandler}
                     style={style}>Toggle Persons</button>
-                { this.state.showPersons ?
-                    <div>
-                        <Person 
-                            name={this.state.persons[0].name} 
-                            age={this.state.persons[0].age} />
-                        <Person 
-                            name={this.state.persons[1].name} 
-                            age={this.state.persons[1].age}
-                            click={this.switchNameHandler.bind(this, "Hazel Hazel")}
-                            changed={this.nameChangedHandler}>I like to run.</Person>
-                        <Person 
-                            name={this.state.persons[2].name} 
-                            age={this.state.persons[2].age} />
-                    </div>
-                    : null
-                }
+                {persons}
             </div>
         )
     }
