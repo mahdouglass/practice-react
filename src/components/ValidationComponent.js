2eedfb1ec6f199c96b9603daf48b6996030a0bfd
Marissa Douglass <mahdouglass@gmail.com>
2019-05-06 22:14:17 -0400

    Add text length logic to validation component

--- src/components/ValidationComponent.js
+++ src/components/ValidationComponent.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-const ValidationComponent = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+const ValidationComponent = (props) => {
+    if (props.textLength < 5) {
+        return <p>Text length is too short.</p>
+    }
+    if (props.textLength > 10) {
+        return <p>Text length is too long.</p>
+    }
+    else {
+        return <p>Text length is just right.</p>
+    }
 }
 export default ValidationComponent;
