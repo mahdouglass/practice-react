eea62314e098c8c5eb5cde5ce5b34a9f46af2ae9
Marissa Douglass <mahdouglass@gmail.com>
2019-05-07 21:43:38 -0400

    Import CharComponent to the app file

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import "./index.css";
 import ValidationComponent from "./components/ValidationComponent";
+import CharComponent from "./components/CharComponent";
 
 class App extends Component {
     state = {
@@ -29,6 +30,7 @@ class App extends Component {
                 <ValidationComponent 
                     value={this.state.textLength}
                 />
+                <CharComponent />
                 <ol>
                     <li>Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
                     <li>Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
