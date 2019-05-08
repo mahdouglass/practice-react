82306ef7e7241e4ea5c0a8fecc81f20449b047a7
Marissa Douglass <mahdouglass@gmail.com>
2019-05-07 21:42:25 -0400

    Add inline styles to span inside CharComponent

--- src/components/CharComponent.js
+++ src/components/CharComponent.js
@@ -1,8 +1,16 @@
 import React from "react";
 
 const CharComponent = () => {
+    const style = {
+        display: "inline-block",
+        padding: "16px",
+        textAlign: "center", 
+        margin: "16px", 
+        border: "1px solid black"
+    }
+
     return (
-        <span></span>
+        <span style={style}></span>
     )
 }
 export default CharComponent;
