6c2ef11ecd273e4441441dc975d74a6063da3403
Marissa Douglass <mahdouglass@gmail.com>
2019-05-26 20:01:01 -0400

    Add console logs for lifecycle updates and fix persons props func errors

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -3,15 +3,33 @@ import React, { Component } from "react";
 import Person from "./Person/Person";
 
 class Persons extends Component {
+    // static getDerivedStateFromProps(props, state) {
+    //     console.log('[Persons.js] getDerivedStateFromProps');
+    //     return state;
+    // }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[Persons.js] shouldComponentUpdate');
+        return true;
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('[Persons.js] getSnapshotBeforeUpdate');
+    }
+
+    componentDidUpdate() {
+        console.log('[Persons.js] componentDidUpdate');
+    }
+
     render() {
         console.log("[Persons.js] rendering...");
         return this.props.persons.map((person, index) => {
             return (
                 <Person 
-                click={() => this.props.deletePersonHandler(index)}
+                click={() => this.props.clicked(index)}
                 name={person.name}
                 age={person.age} 
-                changed={(event) => this.props.nameChangedHandler(event, person.id)} />
+                changed={(event) => this.props.changed(event, person.id)} />
             );
         });
     }
