41e510d5eb930e49c8970163247e6853e180a54a
Marissa Douglass <mahdouglass@gmail.com>
2019-05-28 22:26:33 -0400

    Add useEffect dependency and setTimeout

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -4,7 +4,10 @@ import classes from "./Cockpit.css";
 const cockpit = (props) => {
     useEffect(() => {
         console.log('[Cockpit.js] useEffect');
-    });
+        setTimeout(() => {
+            alert('Saved data');
+        }, 1000);
+    }, [props.persons]);
 
     const assignedClasses = [];
     let btnClass = '';
