d02ed4ff8c7fea5cd691c4a21b3fb6679a470f3e
Marissa Douglass <marissa.douglass@prudential.com>
2019-06-20 20:57:41 -0400

    Initialize auth context function
    Set authenticated to false and added an anonymous login function.

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
-const authContext = React.createContext();
+const authContext = React.createContext({
+    authenticated: false, 
+    login: () => {}, // anonymous function
+});
 
 export default authContext;
