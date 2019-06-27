f0b646d5b476259e22a5887085db57051e6ac7a6
Marissa Douglass <mahdouglass@gmail.com>
2019-06-26 22:35:21 -0400

    Add context to cockpit component and remove prop from app

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import classes from "./Cockpit.css";
+import AuthContext from "../../context/auth-context";
 
 const cockpit = (props) => {
     const toggleButtonRef = useRef(null);
@@ -44,9 +45,9 @@ const cockpit = (props) => {
                 className={btnClass}
                 onClick={props.clicked}
                 >Toggle Persons</button>
-            <button 
-                onClick={props.login}
-                >Login</button>
+            <AuthContext.Consumer>
+               {context => <button onClick={context.login}>Login</button>}
+            </AuthContext.Consumer>
         </div>
     );
 };
