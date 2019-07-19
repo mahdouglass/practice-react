ef9cd51ddcd7e683a3f8e083192165ead393e58a
Marissa Douglass <marissa.douglass@prudential.com>
2019-07-18 22:04:40 -0400

    Add some separation styling

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from "react";
+import classes from "./Layout.css";
 
 const layout = props => {
   return (
     <React.Fragment>
       <div>Toolbar, Side Drawer, Backdrop</div>
-      <main>
+      <main className={classes.content}>
         {props.children}
       </main>
     </React.Fragment>
