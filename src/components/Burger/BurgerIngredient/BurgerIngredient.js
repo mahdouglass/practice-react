0365fcc76dd2f8bd7870695459c84af84d8b6f86
Marissa Douglass <mahdouglass@gmail.com>
2019-07-19 22:49:46 -0400

    Add rest of ingredients and return it

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -15,12 +15,23 @@ const burgerIngredient = (props) => {
                 </div>
             )
             break;
+        case ('meat'):
+            ingredient = <div className="meat"></div>
+            break;
+        case ('cheese'):
+            ingredient = <div className="cheese"></div>
+            break;
+        case ('bacon'):
+            ingredient = <div className="bacon"></div>
+            break;
+        case ('salad'):
+            ingredient = <div className="salad"></div>
+            break;
         default:
+            ingredient = null;
             break;
     }
-    return {
-
-    };
+    return ingredient;
 };
 
 export default burgerIngredient;
