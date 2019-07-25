1c47e7b870810428401d1bc394ca49db1792c6a7
Marissa Douglass <mahdouglass@gmail.com>
2019-07-24 21:19:36 -0400

    Pass ingredients through transform func to display # of its kind

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,16 @@ import React from "react";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import classes from "./Burger.css";
 
-const burger = () => {
+const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients).map(key => {
+        return [...Array(props.ingredients[key])].map((_, i) => {
+            return <BurgerIngredient key={key + i} type={key} />;
+        });
+    });
     return (
         <div className={classes.burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
