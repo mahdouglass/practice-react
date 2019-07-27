47d2b5ff7d34430b8d73286de14b54dbea9723df
Marissa Douglass <mahdouglass@gmail.com>
2019-07-26 22:10:25 -0400

    Start adding styling to single control

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,10 +1,11 @@
 import React from "react";
+import classes from "./BuildControl.css";
 
 const buildControl = (props) => (
-    <div>
-        <div>{props.label}</div>
-        <button>Less</button>
-        <button>More</button>
+    <div className={classes.buildControl}>
+        <div className={classes.label}>{props.label}</div>
+        <button className={classes.less}>Less</button>
+        <button className={classes.more}>More</button>
     </div>
 );
 
