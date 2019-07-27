3d0f8c4dfad8b42b29647792328bc7bd4d1b6f40
Marissa Douglass <mahdouglass@gmail.com>
2019-07-26 22:07:04 -0400

    Create function comp that holds multiple controls

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,10 @@
+import React from "react";
+import BuildControl from "./BuildControl/BuildControl";
+
+const buildControls = () => (
+    <div>
+        <BuildControl />
+    </div>
+);
+
+export default buildControls;
