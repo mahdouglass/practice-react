f3728bc5c511a89148e803e734de328c8d8a1702
Marissa Douglass <marissa.douglass@prudential.com>
2019-08-04 21:47:19 -0400

    Forgot to add key to OrderSummary map

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -3,7 +3,7 @@ import React from "react";
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients)
     .map(igKey => {
-      return <li><span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}</li>
+      return <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}</li>
     });
 
   return (
