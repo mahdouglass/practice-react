87b1d51b7102ad95ce45b3aa1893f3a562303a54
Marissa Douglass <marissa.douglass@prudential.com>
2019-08-06 22:13:11 -0400

    Add more styling on modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -4,7 +4,8 @@ import classes from "./Modal.css";
 const modal = (props) => (
   <div className={classes.modal}
     style={{
-      transform: props.show ? 'translateY(0)' : 'translateY(-100vh)'
+      transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+      opacity: props.show ? '1' : '0'
     }}
   >
     {props.children}
