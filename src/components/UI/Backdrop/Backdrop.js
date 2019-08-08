f4fe0c96e2d61f6f0533f34885b85611bc0a4deb
Marissa Douglass <marissa.douglass@prudential.com>
2019-08-07 21:51:35 -0400

    Import backdrop to modal

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,7 +1,9 @@
 import React from "react";
 
+import classes from "./Backdrop.css";
+
 const backdrop = (props) => (
-  props.show ? <div></div> : null
+  props.show ? <div className={classes.backdrop}></div> : null
 );
 
 export default backdrop;
