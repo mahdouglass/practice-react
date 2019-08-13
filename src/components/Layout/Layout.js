408f2b162989a5ca89049a5b7489973b1bcb1e92
Marissa Douglass <marissa.douglass@prudential.com>
2019-08-12 21:34:49 -0400

    Add toolbar to layout with css updates

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React from "react";
 import classes from "./Layout.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const layout = props => {
   return (
     <React.Fragment>
+      <Toolbar />
       <div>Toolbar, Side Drawer, Backdrop</div>
       <main className={classes.content}>
         {props.children}
