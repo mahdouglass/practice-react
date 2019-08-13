4573796f077678159c6c7960a02a7f8dcde5bd3f
Marissa Douglass <marissa.douglass@prudential.com>
2019-08-12 21:40:32 -0400

    Burger Logo function component was created with new logo image

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import BurgerLogo from "../../assets/images/burger-logo.png";
+
+const logo = () => (
+  <div>
+    <img src={BurgerLogo} alt="Burger Logo" />
+  </div>
+);
+
+export default logo;
