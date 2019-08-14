dc278b47250af7e509603bda09bbd5ccc9b93432
Marissa Douglass <marissa.douglass@prudential.com>
2019-08-13 22:07:41 -0400

    Create ul and li components for nav

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const navigationItem = () => (
+  <li>
+    <a href="/">Link</a>
+  </li>
+);
+
+export default navigationItem;
---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import NavigationItem from "./NavigationItem/NavigationItem";
+
+const navigationItems = () => (
+  <ul>
+    <NavigationItem />
+  </ul>
+);
+
+export default navigationItems;
