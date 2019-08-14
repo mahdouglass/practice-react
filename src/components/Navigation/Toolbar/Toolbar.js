7d4138ff9d3d8b6e706d016a535510dcc9974636
Marissa Douglass <marissa.douglass@prudential.com>
2019-08-13 22:01:37 -0400

    Import logo to navigation

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,10 +1,12 @@
 import React from "react";
+
+import Logo from "../../Logo/Logo";
 import classes from "./Toolbar.css";
 
 const toolbar = () => (
     <header className={classes.toolbar}>
       <div>Menu</div>
-      <div>Logo</div>
+      <Logo />
       <nav>
         ...
       </nav>
