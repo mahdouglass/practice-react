53c7452350b0e5a366416ac3ae100b240f72d4b3
Marissa Douglass <marissa.douglass@prudential.com>
2019-08-16 21:32:53 -0400

    Toolbar and side drawer css now handling logo height

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -7,7 +7,9 @@ import classes from "./SideDrawer.css";
 const sideDrawer = (props) => {
   return (
     <div className={classes.sideDrawer}>
-      <Logo />
+      <div className={classes.logo}>
+        <Logo />
+      </div>
       <nav>
         <NavigationItems />
       </nav>
