df540135de0b95a822da7af44d58d85c48e18ffc
Marissa Douglass <marissa.douglass@prudential.com>
2019-08-18 21:37:58 -0400

    Change side drawer classes based on open and show

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,10 +6,16 @@ import Backdrop from "../../UI/Backdrop/Backdrop";
 import classes from "./SideDrawer.css";
 
 const sideDrawer = (props) => {
+  let attachClasses = [classes.sideDrawer, classes.close];
+
+  if (props.open) {
+    attachClasses = [classes.sideDrawer, classes.open];
+  }
+
   return (
     <React.Fragment>
       <Backdrop show={props.open} clicked={props.close} />
-      <div className={classes.sideDrawer}>
+      <div className={attachClasses.join(' ')}>
         <div className={classes.logo}>
           <Logo />
         </div>
