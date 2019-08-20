a7c1db2cd3b986bfb44f3d2c4f1902938c940c7d
Marissa Douglass <marissa.douglass@prudential.com>
2019-08-19 21:34:30 -0400

    Add toggle functionality to only mobile menu

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,18 +5,27 @@ import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 class Layout extends Component {
   state = {
-    showSideDrawer: true,
+    showSideDrawer: false,
   }
 
   closeSideDrawer = () => {
     this.setState({showSideDrawer: false});
   }
 
+  drawerMenuClick = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !this.state.showSideDrawer };
+    });
+  }
+
   render() {
     return (
       <React.Fragment>
-        <Toolbar />
-        <SideDrawer open={this.state.showSideDrawer} close={this.closeSideDrawer} />
+        <Toolbar drawerMenuClick={this.drawerMenuClick} />
+        <SideDrawer className={classes.mobileOnly}
+          open={this.state.showSideDrawer}
+          close={this.closeSideDrawer} 
+        />
         <div>Toolbar, Side Drawer, Backdrop</div>
         <main className={classes.content}>
           {this.props.children}
