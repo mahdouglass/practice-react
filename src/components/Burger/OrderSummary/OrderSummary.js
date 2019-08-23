35b06257293f7e76089d7e22648991b691a1dad6
Marissa Douglass <mahdouglass@gmail.com>
2019-08-22 23:02:21 -0400

    Change order summary function to class to use lifecycyle

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+  componentWillMount() {
+    console.log('[Order Summary] component will mount');
+  }
+
+  render () {
+    const ingredientSummary = Object.keys(this.props.ingredients)
     .map(igKey => {
       return (
         <li key={igKey}>
-          <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
+          <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}
         </li>
       );
     });
 
-  return (
-    <React.Fragment>
-      <h3>Your Order</h3>
-      <p>Your burger includes the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p>Total: <strong>${props.price.toFixed(2)}</strong></p>
-      <p>Continue to checkout?</p>
-      <Button btnType="danger" clicked={props.cancelOrder}>Cancel</Button>
-      <Button btnType="success" clicked={props.continueOrder}>Continue</Button>
-    </React.Fragment>
-  );
+    return (
+      <React.Fragment>
+        <h3>Your Order</h3>
+        <p>Your burger includes the following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p>Total: <strong>${this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to checkout?</p>
+        <Button btnType="danger" clicked={this.props.cancelOrder}>Cancel</Button>
+        <Button btnType="success" clicked={this.props.continueOrder}>Continue</Button>
+      </React.Fragment>
+    );
+  }
 };
 
-export default orderSummary;
+export default OrderSummary;
