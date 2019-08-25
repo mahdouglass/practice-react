57371762488629816c5cbe47bf54fdad9b5eb579
Marissa Douglass <mahdouglass@gmail.com>
2019-08-24 20:42:18 -0400

    Add lifecycle to modal to not update continuously

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -4,6 +4,14 @@ import classes from "./Modal.css";
 import Backdrop from "../Backdrop/Backdrop";
 
 class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  componentWillUpdate() {
+    console.log('[Modal] will update');
+  }
+
   render () {
     return (
       <React.Fragment>
