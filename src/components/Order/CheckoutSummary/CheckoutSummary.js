a39877f6a16aa4ae03a73f9282ae5fd8f796d6fa
Marissa Douglass <mahdouglass@gmail.com>
2019-10-06 21:09:33 -0400

    Create rough checkout components

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const checkoutSummary = (props) => {
+    return (
+        <div>
+            <h1>We hope you enjoy!</h1>
+            <div>
+                
+            </div>
+        </div>
+    );
+}
+
+export default checkoutSummary;
