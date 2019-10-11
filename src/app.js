b6c2b9971e3e337357db55e531116623462bd357
Marissa Douglass <mahdouglass@gmail.com>
2019-10-10 20:05:01 -0400

    Hardcoded checkout to app

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
+
 import Layout from "./containers/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
     render () {
         return (
             <React.Fragment>
                 <Layout>
-                    <BurgerBuilder></BurgerBuilder>
+                    <BurgerBuilder />
+                    <Checkout />
                 </Layout>
             </React.Fragment>
         )
