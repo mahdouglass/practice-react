cee1347b7d91ab99a05cc55ba36e6c0daed04f6b
Marissa Douglass <mahdouglass@gmail.com>
2019-11-12 20:32:13 -0500

    Add spinner if form is errored

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import axios from "../../../axios-orders";
 
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 
 class ContactData extends Component {
     state = {
@@ -41,16 +42,22 @@ class ContactData extends Component {
     }
 
     render () {
+        let form = (
+            <form>
+                <input className={classes.input} type="text" name="name" placeholder="Name" />
+                <input className={classes.input} type="text" name="email" placeholder="Email" />
+                <input className={classes.input}  type="text" name="street" placeholder="Street" />
+                <input className={classes.input} type="text" name="postal" placeholder="Postal Code" />
+                <Button btnType="success"clicked={this.orderHandler}>Order Now</Button>
+            </form>
+        );
+        if (this.state.loading) {
+            form = <Spinner />;
+        }
         return (
             <div className={classes.contactData}>
                 <h4>Enter your contact information</h4>
-                <form>
-                    <input className={classes.input} type="text" name="name" placeholder="Name" />
-                    <input className={classes.input} type="text" name="email" placeholder="Email" />
-                    <input className={classes.input}  type="text" name="street" placeholder="Street" />
-                    <input className={classes.input} type="text" name="postal" placeholder="Postal Code" />
-                    <Button btnType="success"clicked={this.orderHandler}>Order Now</Button>
-                </form>
+                {form}
             </div>
         );
     }
