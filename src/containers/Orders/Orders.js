f2be3bb2c12534cd0c92dba88a0e81aecc7fbb8d
Marissa Douglass <mahdouglass@gmail.com>
2019-11-13 20:02:15 -0500

    Add basic orders class

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Orders extends Component {
+    render () {
+        return (
+            <React.Fragment>
+                
+            </React.Fragment>
+        );
+    }
+}
+
+export default Orders;
