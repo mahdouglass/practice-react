1ac662811eb6d2da23518098bcd8b6c29f8b774c
Marissa Douglass <mahdouglass@gmail.com>
2019-11-14 18:55:05 -0500

    Change nav a tags to NavLinks

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { NavLink } from 'react-router-dom';
 
 import classes from "./NavigationItem.css";
 
 const navigationItem = (props) => (
   <li className={classes.navItem}>
-    <a href={props.link} className={props.active ? classes.active : null}>{props.children}</a>
+    <NavLink to={props.link}>{props.children}</NavLink>
   </li>
 );
 
