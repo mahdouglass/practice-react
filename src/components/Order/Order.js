93d7440825d667be1dc3eb818228b4b3a88ea18f
Marissa Douglass <mahdouglass@gmail.com>
2019-11-13 20:06:37 -0500

    Add styling to single order view

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import classes from './Order.css';
+
 const order = (props) => (
-    <div>
+    <div className={classes.order}>
         <p>Ingredients: Salad (1)</p>
         <p>Price: <strong>USD $5.45</strong></p>
     </div>
