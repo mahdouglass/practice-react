a736911844f03f2a87c99adf9f1b0444dcbecc96
Marissa Douglass <mahdouglass@gmail.com>
2019-11-14 19:00:19 -0500

    Fix active class styling on NavLinks

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -5,7 +5,7 @@ import classes from "./NavigationItem.css";
 
 const navigationItem = (props) => (
   <li className={classes.navItem}>
-    <NavLink to={props.link}>{props.children}</NavLink>
+    <NavLink to={props.link} activeClassName={classes.active} exact>{props.children}</NavLink>
   </li>
 );
 
