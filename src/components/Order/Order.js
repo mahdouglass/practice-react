e48ca6dab5c24dddc9e9189a9588e023f5121944
Marissa Douglass <mahdouglass@gmail.com>
2019-11-26 20:47:36 -0500

    Pass price dynamically to each order

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import classes from './Order.css';
 
-const order = (props) => (
-    <div className={classes.order}>
-        <p>Ingredients: Salad (1)</p>
-        <p>Price: <strong>USD $5.45</strong></p>
-    </div>
-);
+const order = (props) => {
+    return (
+        <div className={classes.order}>
+            <p>Ingredients: </p>
+            <p>Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
+        </div>
+    );
+};
 
 export default order;
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -29,8 +29,13 @@ class Orders extends Component {
     render () {
         return (
             <React.Fragment>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => (
+                    <Order 
+                        key={order.id}
+                        ingredients={order.ingredients}
+                        price={order.price}
+                    />
+                ))}
             </React.Fragment>
         );
     }
